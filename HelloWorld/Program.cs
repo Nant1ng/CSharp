8bd32a916eba81b9624ab8ef9ac74a3e4193d964
Nant1ng <andressantana99@hotmail.se>
2023-09-06 11:31:45 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter du?");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"Hejsan,  + {name}");
+
+        }
+    }
+}
