e5934928a23d5a9345166c431842ce74130f9d9b
Nant1ng <andressantana99@hotmail.se>
2023-09-21 11:05:04 +0200

    Done

---
+++ EnumExercises/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace EnumExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool looping = true;
+
+            while (looping)
+            {
+                Console.Write("Give a number between 1 and 12 and I'll tell you what month it is: ");
+                string? inputValue = Console.ReadLine();
+
+                if (int.TryParse(inputValue, out int month))
+                {
+                    if (month >= 1 && month <= 12)
+                    {
+                        EnumMonth monthEnum = (EnumMonth)month;
+                        Console.WriteLine($"{month} = {monthEnum}");
+                        looping = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You need to answer with a number between 1 and 12.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You can't answer with a letter, you need to answer with a number between 1 and 12");
+                }
+            }
+        }
+    }
+}
