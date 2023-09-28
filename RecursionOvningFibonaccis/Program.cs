95d621b3223ccd9c459fb704d367f56950c81b37
Nant1ng <andressantana99@hotmail.se>
2023-09-28 09:21:35 +0200

    Done

---
+++ RecursionOvningFibonaccis/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace RecursionOvningFibonaccis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\x1b[6mFibonaccis\x1b[0m");
+
+            for (int num = 0; num <= 10; num++)
+            {
+                Console.WriteLine(Fibonaccis(num));
+            }
+        }
+        static int Fibonaccis(int num)
+        {
+            int numOne = 0;
+            int numTwo = 1;
+            int result = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                result = numOne + numTwo;
+                numOne = numTwo;
+                numTwo = result;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SalaryCalculator/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace SalaryCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats your name, age and worked hours, seprate them with ,");
+            Console.WriteLine("Name, Age and Worked Hours: ");
+
+            string? inputValue = Console.ReadLine();
+
+            if (inputValue != null)
+            {
+                string[] inputValues = inputValue.Split(", ");
+                string name = inputValues[0];
+                string ageValue = inputValues[1];
+                string hoursValue = inputValues[2];
+
+                bool ageCheck = int.TryParse(ageValue, out int age);
+                bool hoursCheck = int.TryParse(hoursValue, out int hours);
+
+                if (name != null && ageCheck && hoursCheck)
+                {
+                    double salary = SalaryCounter(name, age, hours);
+                    if (salary < 0)
+                    {
+                        Console.WriteLine($"You owe us money Rickard: {salary}kr");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Your salary is: {salary}kr");
+                    }
+                }
+            }
+        }
+        static double SalaryCounter(string name, int age, int hours)
+
+        {
+            string mvp = "Richard";
+            string MVP = "Andrés";
+            double salaryPerHours;
+
+            if (age < 30)
+            {
+                salaryPerHours = 100;
+            }
+            else if (age >= 30 && age <= 50)
+            {
+                salaryPerHours = 120;
+            }
+            else
+            {
+                salaryPerHours = 130;
+            }
+
+            if (name == mvp)
+            {
+                salaryPerHours *= -1;
+            }
+            else if (name == MVP)
+            {
+                salaryPerHours *= 1.5;
+            }
+
+            double salary = salaryPerHours * hours;
+            return salary;
+        }
+    }
+}
