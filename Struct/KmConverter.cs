5fb76f46f3724d4710662c50eb3c91b0c5fe2ad4
Nant1ng <andressantana99@hotmail.se>
2023-10-10 13:34:19 +0200

    Km Converter Done

---
+++ Struct/KmConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace Struct
+{
+    public struct KmConverter
+    {
+        private float _km;
+        private float _miles;
+
+        public KmConverter(float km)
+        {
+            _km = km;
+        }
+
+        public float Km { get { return _km; } set { _km = value; } }
+
+        public float Miles { get { return _miles; } set { _miles = value; } }
+
+        public void ConvertedToMiles()
+        {
+            Miles = _km * 0.621371192f;
+        }
+
+        //public float ConvertedToMiles()
+        //{
+        //    float convertedValue = _km * 1.621371192f;
+
+        //    return convertedValue;
+        //}
+    }
+}
---
+++ Struct/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Struct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I can convert Km to Miles.");
+            Console.Write("Tell me the value you want to convert: ");
+
+            string? inputValue = Console.ReadLine();
+
+            float.TryParse(inputValue, out float km);
+
+            KmConverter converter = new KmConverter(km);
+            converter.ConvertedToMiles();
+
+            Console.WriteLine($"{converter.Km}");
+            Console.WriteLine($"{converter.Miles}");
+        }
+    }
+}
