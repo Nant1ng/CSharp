695a1f562cd401012632dc4984eb6c0a2f983775
Nant1ng <andressantana99@hotmail.se>
2023-11-21 13:26:45 +0100

    Lib and CRUD

---
+++ LibraryAndServices/Program.cs
@@ -0,0 +1,91 @@
+﻿using MyClassLibrary;
+using MyClassLibrary.Services;
+
+namespace LibraryAndServices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Data.SeedData();
+            var run = true;
+            var customerService = new CustomerService();
+
+            while (run)
+            {
+                Console.Clear();
+                Console.WriteLine("1: Create a customer");
+                Console.WriteLine("2: Read a customer (with Id)");
+                Console.WriteLine("3: Read all customers");
+                Console.WriteLine("4: Update a customer");
+                Console.WriteLine("5: Delete a customer");
+                Console.WriteLine("0: Exit");
+                Console.Write("\nInput: ");
+
+                if (int.TryParse(Console.ReadLine(), out var userChoice))
+                {
+                    switch (userChoice)
+                    {
+                        case 1:
+                            customerService.Create();
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Id of customer: ");
+                            var customerIdReadOne = Convert.ToInt32(Console.ReadLine());
+                            var readOne = customerService.ReadOne(customerIdReadOne);
+
+                            if (readOne != null)
+                            {
+                                Console.WriteLine(
+                                $"{readOne.CustomerId}: {readOne.Name}, {readOne.Email}");
+                                Console.WriteLine("Press any key to continue");
+                                Console.ReadLine();
+                            }
+
+                            break;
+
+                        case 3:
+                            var customers = customerService.ReadAll();
+
+                            foreach (var c in customers)
+                            {
+                                Console.WriteLine(
+                                    $"{c.CustomerId}: {c.Name}, {c.Email}");
+                            }
+
+                            Console.WriteLine("Press any key to continue");
+                            Console.ReadLine();
+
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Id of customer you want to update: ");
+                            var customerIdUpdate = Convert.ToInt32(Console.ReadLine());
+
+                            customerService.Update(customerIdUpdate);
+                            break;
+
+                        case 5:
+                            Console.WriteLine("Id of customer you want to delete: ");
+                            var customerIdDelete = Convert.ToInt32(Console.ReadLine());
+                            customerService.Delete(customerIdDelete);
+                            break;
+
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ MyClassLibrary/Services/CustomerService.cs
@@ -0,0 +1,70 @@
+﻿namespace MyClassLibrary.Services
+{
+    public class CustomerService
+    {
+        public List<Customer> Customers { get; private set; }
+
+        public CustomerService()
+        {
+            Customers = Data.Customers ?? new List<Customer>();
+        }
+
+        public Customer Create()
+        {
+            Console.WriteLine("Vad heter din nya kund?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Emailadress?");
+            var email = Console.ReadLine();
+
+            ++Data.IdSeed;
+            var customer = new Customer(Data.IdSeed, name, email);
+            Data.Customers.Add(customer);
+
+            return customer;
+        }
+
+        public Customer ReadOne(int customerId)
+        {
+            return Data.Customers.Find(c => c.CustomerId == customerId);
+        }
+
+        public List<Customer> ReadAll()
+        {
+            return Data.Customers.ToList();
+        }
+
+        public void Update(int customerId)
+        {
+            var customerToUpdate = Data.Customers.Find(c => c.CustomerId == customerId);
+
+            if (customerToUpdate == null)
+            {
+                Console.WriteLine("Kunden gick ej att hitta.");
+                return;
+            }
+
+            Console.WriteLine("Uppdatera namnet på din kund");
+            var name = Console.ReadLine();
+            Console.WriteLine("Uppdatera Emailadressen på din kund");
+            var email = Console.ReadLine();
+
+            customerToUpdate.Name = name;
+            customerToUpdate.Email = email;
+
+            Console.WriteLine("Customer updated successfully");
+        }
+
+        public void Delete(int customerId)
+        {
+            var customerToDelete = Data.Customers.Find(c => c.CustomerId == customerId);
+            if (customerToDelete == null)
+            {
+                Console.WriteLine("Kunden gick ej att hitta.");
+                return;
+            }
+            Data.Customers.Remove(customerToDelete);
+            Console.WriteLine("Kunden Raderades");
+        }
+    }
+}
